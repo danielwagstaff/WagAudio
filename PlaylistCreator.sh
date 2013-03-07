f103e9ad4ebca7eccfd8f0fb36f8bbb8e015d0ae
waggy55 <wagstaff.daniel@gmail.com>
2013-03-07 09:10:19 +0000

    PlaylistCreator.sh-Updated_Output_File

--- PlaylistCreator.sh
+++ PlaylistCreator.sh
@@ -10,5 +10,5 @@ nFiles=$(ls "$pldir" | wc -l) #The number of files in the directory
 for i in $(seq 1 $nFiles)
 do
     filenames[$i]=$(ls "$pldir" | head -$i | tail -1)
-    echo $pldir${filenames[$i]} >> ./$outputfile.pl
+    echo $pldir${filenames[$i]} >> $outputfile.pl
 done
